754cd7f15d7c532b76dac3742422cbff82bb5000
Jaegarr <berke.s.bayram@gmail.com>
2025-06-30 02:09:01 +0100

    RPM Sweep added

--- v0.1_Proof_of_Concept/Virtual_Engine_Test_Bench_v0.1.py
+++ v0.1_Proof_of_Concept/Virtual_Engine_Test_Bench_v0.1.py
@@ -1,4 +1,26 @@
 from engine_model import calculate_torque
-
-x = calculate_torque(1000, 100)
-print('Your torque is',x)
+while True:
+    try:
+        print('Enter throttle position:')
+        throttle = float(input())
+        if throttle < 0.0 or throttle > 1.0:
+            print('Throttle position must be between 0 and 1. Please enter a throttle position:')
+            continue
+        break
+    except:
+        print('Invalid input. Throttle position must be between 0 and 1')
+'''
+while True:
+    try:
+        print('Enter engine RPM:')
+        rpm = float(input())
+        if rpm < 0.0 or rpm > 15000.0:
+            print('Engine RPM must be between 0 and 15000. Please enteR engine RPM:')
+            continue
+        break
+    except:
+        print('Invalid input. Engine RPM must be between 0 and 15000')
+'''
+for rpm in range(1000,8000,500): #RPM Sweep
+     x = calculate_torque(rpm, throttle)
+     print(f'Your torque at {rpm} RPM is {x}')
