2e49298340d9f143205d73c5fc051e03c5fdea63
Jaegarr <berke.s.bayram@gmail.com>
2025-07-11 06:49:59 +0100

    Update engine_model.py

--- v0.1_Proof_of_Concept/engine_model.py
+++ v0.1_Proof_of_Concept/engine_model.py
@@ -1,10 +1,23 @@
 import math
-def calculate_torque(rpm, throttle):
-    a = 0.05
-    b = 0.00001
-    #a and b are arbitrary
-    torque = a * throttle * rpm - b * rpm**2 #In N
-    return max(torque,0)
+def calculate_air_mass_flow(rpm, displacement_l, ve, rho = 1.22588):   
+    '''
+    Calculate the air mass flow rate (kg/s) into a 4-stroke engine.
+
+    Parameters:
+        rpm (float): Engine speed in revolutions per minute
+        displacement_l (float): Engine displacement in liters
+        VE (float): Volumetric efficiency (0-1.2)
+        air_density (float): Air density in kg/m³ (default 1.225)
+    Returns:
+        float: Air mass flow rate in kg/s
+    '''
+    displacement_m = displacement_l/1e3
+    mdot = (displacement_m*1e-3)*ve*rpm*rho/(2*60)
+    return mdot
+def calculate_torque(rpm, mdotAir, throttle,AFR = 14.7, LHV = 44e6, eff = 0.3):
+    mdotFuel = mdotAir*throttle/AFR
+    torque = mdotFuel*LHV*eff/(rpm*2*math.pi/60) # In Nm
+    return torque
 def calculate_power(rpm, torque):
     power = (torque*rpm*2*math.pi/60)/1000 # In kW
     return power
