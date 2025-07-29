1326eea9869d3c5e80da8d8041b09e46a432ebf6
Jaegarr <berke.s.bayram@gmail.com>
2025-07-29 12:42:22 +0100

    PMEP Introduced

--- v0.5_Steady_State_Mapping/Calibration.py
+++ v0.5_Steady_State_Mapping/Calibration.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import numpy as np
+import math
 lambda_target_map = pd.DataFrame({'RPM': [1000, 2000, 3000, 4000, 5000, 6000, 7000],'Lambda': [1.0, 1.0, 0.95, 0.92, 0.90, 0.88, 0.88]})
 def get_target_lambda(rpm,AFR = 14.7):
    target_lambda = float(np.interp(rpm,lambda_target_map['RPM'],lambda_target_map['Lambda']))
    target_AFR = AFR*target_lambda
    return target_AFR
-def calculate_fmep(rpm):
+def calculate_fmep(rpm,displacement_l):
    fmep = 0.25 + 0.02*rpm/1000 + 0.03*(rpm/1000)**2
+   displacement_m3 = displacement_l/1e3
    fmep_pa = fmep*1e5
-   return fmep_pa
-
+   fmep_nm = fmep_pa * displacement_m3/(4*math.pi)
+   return fmep_nm
+def calculate_pmep(rpm, displacement_l):
+   pmep = 0.02 + 0.00001*rpm
+   pmep_pa = pmep*1e5
+   displacement_m3 = displacement_l/1e3
+   pmep_nm = pmep_pa*displacement_m3/(4*math.pi)
+   return pmep_nm
     
--- v0.5_Steady_State_Mapping/engine_model.py
+++ v0.5_Steady_State_Mapping/engine_model.py
@@ -12,14 +12,15 @@ def calculate_air_mass_flow(rpm, displacement_l, ve, rho = 1.22588):
     Returns:
         float: Air mass flow rate in kg/s
     '''
-    displacement_m = displacement_l/1e3
-    mdot = displacement_m*ve*rpm*rho/(2*60)
+    displacement_m3 = displacement_l/1e3
+    mdot = displacement_m3*ve*rpm*rho/(2*60)
     return mdot
 def calculate_torque(rpm, throttle, mdotAir, displacement_l, LHV = 44e6, eff = 0.3):
     mdotFuel = mdotAir*throttle/cal.get_target_lambda(rpm)
     gross_torque = mdotFuel*LHV*eff/(rpm*2*math.pi/60) # In Nm
-    torque_fmep = cal.calculate_fmep(rpm)*displacement_l/1e3/(2*math.pi)/2 # Last 1/2 is for 4-stroke engines(0.5 cycle)
-    net_torque = gross_torque - torque_fmep
+    torque_fmep = cal.calculate_fmep(rpm, displacement_l)
+    torque_pmep = cal.calculate_pmep(rpm, displacement_l)
+    net_torque = gross_torque - torque_fmep - torque_pmep
     return max(net_torque,0) # I am not confident with myself sometimes
 def calculate_power(rpm, torque):
     power = (torque*rpm*2*math.pi/60)/1000 # In kW
